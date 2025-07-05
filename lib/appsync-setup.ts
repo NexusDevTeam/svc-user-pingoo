987b8b91c831e14cbaddec6bbab5b64068cac935
antfconeto <antfcone@gmail.com>
2025-07-05 16:37:19 -0300

    fix: :green_heart: Atualiza a expiração da chave da API do Appsync de 23 horas para 1 dia.

--- lib/appsync-setup.ts
+++ lib/appsync-setup.ts
@@ -55,7 +55,7 @@ export class AppsyncSetup {
     new appsync.CfnApiKey(this.stack, "userApiKey", {
       apiId: this.userApi.attrApiId,
       description: "userApiKey",
-      expires: Duration.hours(23).toSeconds()
+      expires: Duration.days(1).toSeconds()
     })
 
     new logs.LogGroup(this.stack, "userApiLogsGroup", {
